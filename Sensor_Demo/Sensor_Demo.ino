97c2615e9cad54d6f47ac16574315dadb79a36c3
milfordn <milfordn@oregonstate.edu>
2019-04-25 20:59:40 -0700

    sensor demo

--- Sensor_Demo/Sensor_Demo.ino
+++ Sensor_Demo/Sensor_Demo.ino
@@ -1,4 +1,4 @@
-#include <FastGPIO.h>
+//#include <FastGPIO.h>
 
 //connect motor controller pins to Arduino digital pins
 // motor one
@@ -18,19 +18,24 @@ long encoderTime = 0;
 long encoderTimeLast = 0;
 int ticks = 0;
 bool encoderUpdate = false;
-int8_t dir = 0;
+int8_t encoderDir = 0;
+int motorDir = 1;
 
 void setup() {
   // put your setup code here, to run once:
  // set all the motor control pins to outputs
   Serial.begin(9600);
-  Serial.print("Starting!");
+  Serial.println("Starting!");
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(encoder1a, INPUT);
   pinMode(encoder1b, INPUT);
-  digitalWrite(encoder1b, 0);
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(encoder1b, LOW);
+  analogWrite(enA, 90);
   attachInterrupt(digitalPinToInterrupt(encoder1a), updateMeasurements, CHANGE);
   encoderTime = micros();
   encoderTimeLast = encoderTime - 1000;
@@ -39,9 +44,19 @@ void setup() {
 void updateMeasurements(){
   encoderTimeLast = encoderTime;
   encoderTime = micros();
-  dir = (FastGPIO::Pin<encoder1b>::isInputHigh() << 1) - 1;
-  ticks += dir;
+  encoderDir = (digitalRead(encoder1b) * 2) - 1;
+  ticks += encoderDir;
   encoderUpdate = true;
+  if(ticks < -32){
+//    analogWrite(enA, 0);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  }
+//  if(ticks < -16){
+//    digitalWrite(in1, LOW);
+//    digitalWrite(in2, HIGH);
+//    ticks = 0;
+//  }
 }
 
 void loop() {
@@ -49,7 +64,7 @@ void loop() {
   if(encoderUpdate){
     encoderUpdate = false;
     Serial.print("Encoder Moved  at ");
-    float measuredSpeed = dir * 45. / ((encoderTime - encoderTimeLast) / 1000000.);
+    float measuredSpeed = encoderDir * 45. / ((encoderTime - encoderTimeLast) / 1000000.);
     Serial.print(measuredSpeed);
     Serial.print(" Degrees per second, ");
     Serial.print(ticks * 45);

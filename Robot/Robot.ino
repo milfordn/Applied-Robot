ea2b45ed7164c2fc97d54514c5ffb7e61205563a
edwarda3 <edwarda3@oregonstate.edu>
2019-04-26 13:00:51 -0700

    Made controls to move motor

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,20 +1,42 @@
-#include <L298N.h>
-
-#define EN1 3
-#define EN2 4
-#define IN1_1 8
-#define IN1_2 9
-#define IN2_1 10
+#define EN1 10
+#define EN2 13
+#define IN1_1 9
+#define IN1_2 8
+#define IN2_1 12
 #define IN2_2 11
 
-#define LED1 10
-#define LED2 9
-#define LED3 8
-#define LED4 7
+#define ENC_1A 2
+#define ENC_1B 6
+
+class Wheel{
+    int pin_en;
+    int pin_in1;
+    int pin_in2;
+    int pin_enc_a;
+    int pin_enc_b;
+
+    long encoderTime = 0;
+    long encoderTimeLast = 0;
+    int ticks = 0;
+    bool encoderUpdate = false;
+    int encoderDir = 0;
+    int motorDir = 1;
+
+    float speed;
+public:
+    void updateWheel();
+
+    void drive(int dir, int vel);
+
+    Wheel(int en, int in1, int in2, int enc_a, int enc_b);
+
+};
 
 char action;
 bool moving = false;
 bool obstacle = false;
+
+Wheel leftwheel(EN1, IN1_1, IN1_2, ENC_1A, ENC_1B);
 
 
 void setup() {
@@ -22,11 +44,6 @@ void setup() {
 
   Serial.begin(115200);
   Serial.println("Connected");
-
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
-  pinMode(LED4,OUTPUT);
 }
 
 void loop() {
@@ -65,22 +82,7 @@ void loop() {
       
       break;
 
-    //LED exhibition
-    case 'd':
-      Serial.println("LED BOUNCE ON");
-      ledbounce(10);
 
-      break;
-    case 's':
-      Serial.println("LED LINE ON");
-      ledline(10);
-
-      break;
-    case 'a':
-      Serial.println("LED FLICKER ON");
-      altflicker(5);
-
-      break;
     case 'i':
       int integer = Serial.parseInt();
       Serial.print("Got integer: ");
@@ -94,99 +96,61 @@ void loop() {
 
 void move(){
   if(moving){ //IF WE SHOULD BE MOVING
-    digitalWrite(LED1,HIGH);
-    if(!obstacle){ //There is no obstacle
-      digitalWrite(LED3,LOW);
-      digitalWrite(LED4,HIGH);
-      
+    if(!obstacle){
+    
       //MOTOR LOGIC HERE
-      
+      leftwheel.drive(0,100);
     } else { //There is an obstacle
-      digitalWrite(LED3,HIGH);
-      digitalWrite(LED4,LOW);
-
+      
       //STOP MOTORS OR SWERVE
+      leftwheel.drive(1,100);
     }
-  } else{ //IF NOT
-    digitalWrite(LED1,LOW);
-    digitalWrite(LED3,LOW);
-    digitalWrite(LED4,LOW);
-
+  } else { //IF NOT
+    
     //Turn off motors
+    leftwheel.drive(0,0);
   }
 }
 
-void ledline(int n){
-  int d = 50;
-  for(int i=0; i<n; i++){
-    digitalWrite(LED1,HIGH);
-    delay(d);
-    digitalWrite(LED2,HIGH);
-    delay(d);
-    digitalWrite(LED3,HIGH);
-    digitalWrite(LED1,LOW);
-    delay(d);
-    digitalWrite(LED4,HIGH);
-    digitalWrite(LED2,LOW);
-    delay(d);
-    digitalWrite(LED3,LOW);
-    delay(d);
-    digitalWrite(LED4,LOW);
-  }
+
+void Wheel::updateWheel(){
+    encoderTimeLast = encoderTime;
+    encoderTime = micros();
+    encoderDir = (digitalRead(pin_enc_a) * 2) - 1;
+    ticks += encoderDir;
+    encoderUpdate = true;
+
+    speed = encoderDir * 45. / ((encoderTime - encoderTimeLast) / 1000000.);
 }
 
-void ledbounce(int n){
-  int d = 50;
-  for(int i=0; i<n; i++){
-    digitalWrite(LED1,HIGH);
-    delay(d);
-    digitalWrite(LED2,HIGH);
-    delay(d);
-    digitalWrite(LED3,HIGH);
-    digitalWrite(LED1,LOW);
-    delay(d);
-    digitalWrite(LED4,HIGH);
-    digitalWrite(LED2,LOW);
-    delay(d);
-    digitalWrite(LED3,LOW);
-    delay(d);
-    
-    digitalWrite(LED3,HIGH);
-    delay(d);
-    digitalWrite(LED2,HIGH);
-    digitalWrite(LED4,LOW);
-    delay(d);
-    digitalWrite(LED1,HIGH);
-    digitalWrite(LED3,LOW);
-    delay(d);
-    digitalWrite(LED2,LOW);
-    delay(d);
-    digitalWrite(LED1,LOW);
-  }
+void Wheel::drive(int dir, int vel){
+    if(dir){
+    digitalWrite(pin_in1,HIGH);
+    digitalWrite(pin_in2,LOW);
+    }
+    else{
+    digitalWrite(pin_in1,HIGH);
+    digitalWrite(pin_in2,LOW);
+    }
+    analogWrite(pin_en,vel);
 }
 
-void altflicker(int n){
-  int d = 250;
-  for(int i=0;i<n;i++){
-    digitalWrite(LED2,LOW);
-    digitalWrite(LED4,LOW);
-    digitalWrite(LED1,HIGH);
-    digitalWrite(LED3,HIGH);
-    delay(d);
+Wheel::Wheel(int en, int in1, int in2, int enc_a, int enc_b){
+    pin_en = en;
+    pin_in1 = in1;
+    pin_in2 = in2;
+    pin_enc_a = enc_a;
+    pin_enc_b = enc_b;
 
-    digitalWrite(LED2,HIGH);
-    digitalWrite(LED4,HIGH);
-    digitalWrite(LED1,LOW);
-    digitalWrite(LED3,LOW);
-    delay(d);
-    
-  }
-  off();
-}
+    pinMode(pin_en, OUTPUT);
+    pinMode(pin_in1, OUTPUT);
+    pinMode(pin_in2, OUTPUT);
+    pinMode(pin_enc_a, INPUT);
+    pinMode(pin_enc_b, INPUT);
 
-void off(){
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,LOW);
-  digitalWrite(LED4,LOW);
+    analogWrite(pin_en, 0);
+
+    digitalWrite(pin_enc_b, LOW);
+    encoderTime = micros();
+    encoderTimeLast = encoderTime - 1000;
 }

89f1e80112481ba79777a2f0f72c483a52de55e8
edwarda3 <alexedwards31@gmail.com>
2019-05-04 12:23:39 -0700

    Send motor controls to server

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,6 +6,35 @@ $(".actionbutton").each(function(index){
 	});
 });
 
+var joystick	= new VirtualJoystick({
+    container	: document.getElementById('container'),
+    mouseSupport	: true,
+    deadzoneX : 20,
+    deadzoneY : 20,
+    motorAmp : 2,
+});
+var lastm1 = 0;
+var lastm2 = 0;
+setInterval(function(){
+    var m1 = joystick.motorLeft();
+	var m2 = joystick.motorRight();
+	if(m1!=lastm1 || m2!=lastm2){
+		lastm1=m1;
+		lastm2=m2;
+		sendMotorSignal(lastm1,lastm2);
+	} 
+}, 1/30 * 1000);
+
+
+
+//------
+
+function sendMotorSignal(left,right){
+	//console.log(`L:${left}, R:${right}`);
+	var str = `ml${left}r${right}`;
+	socket.emit('binaryaction',str)
+}
+
 socket.emit('getlatestlog');
 
 socket.on('logmessage',function(message){

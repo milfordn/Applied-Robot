ad04e4aab6bb3d59d7a7193595425618c1359d59
edwarda3 <alexedwards31@gmail.com>
2019-05-21 16:09:47 -0700

    update pin numbers with Mega

--- Robot/Mapping.h
+++ Robot/Mapping.h
@@ -1,21 +1,13 @@
 #ifndef MAPPING_H
 #define MAPPING_H
 
-/* No longer used
-#define PIN_DRIVE_LEFT_EN 11
-#define PIN_DRIVE_LEFT_IN1 13
-#define PIN_DRIVE_LEFT_IN2 12
-#define PIN_DRIVE_RIGHT_EN 9
-#define PIN_DRIVE_RIGHT_IN1 8
-#define PIN_DRIVE_RIGHT_IN2 10
-*/
+#define PIN_DRIVE_LEFT_PWM 6
+#define PIN_DRIVE_RIGHT_PWM 5
 
-/* No longer used
-#define PIN_ENCODER_LEFT_A 0
-#define PIN_ENCODER_LEFT_B 0
-#define PIN_ENCODER_RIGHT_A 0
-#define PIN_ENCODER_RIGHT_B 0
-*/
+#define PIN_ENCODER_LEFT_A 30
+#define PIN_ENCODER_LEFT_B 31
+#define PIN_ENCODER_RIGHT_A 32
+#define PIN_ENCODER_RIGHT_B 33
 
 #define PIN_SWITCH_LATCH 7
 #define PIN_MOTOR_LATCH_EN 11
@@ -26,7 +18,7 @@
 #define PIN_MOTOR_CATAPULT_IN1 8
 #define PIN_MOTOR_CATAPULT_IN2 10
 
-#define PIN_SERVO_LOAD 6
+#define PIN_SERVO_LOAD 3
 
 /* No longer used
 #define PIN_DEBUG_LED_1 10
